9793d11019e19d194c08df2b31f9c8a34b996b40
Gabriel Gagnon <gg@Gabriels-MacBook-Pro.local>
2020-03-05 12:02:01 -0800

    added submission class, corrected locate by name

--- retrieve_gradebook.py
+++ retrieve_gradebook.py
@@ -25,8 +25,11 @@ class ClassMasteryData:
     def get_class_id(self):
         my_courses = get_courses()
         for course in my_courses:
-            if(json.dumps(course[0]).find(self.class_name)):
-              self.id = json.dumps(course[1]).strip('"')
+            print("current course: ", course)
+            if(json.dumps(course[0]).find(self.class_name)!= -1):
+                print(self.class_name, " found in string: ", json.dumps(course[0])) 
+                print("accessed course name: ", course[0], "class name: ", self.class_name)
+                self.id = json.dumps(course[1]).strip('"')
         return self.id
         
     def get_coursework(self):
@@ -38,6 +41,70 @@ class ClassMasteryData:
         coursework_id_i = [coursework_id[0]['courseWork'][0]['id']]
         self.submissions = get_submissions([self.id], coursework_id_i)
         return self.submissions
+
+class Classwork(ClassMasteryData):
+    def __init__(self, class_name):
+        ClassMasteryData.__init__(self, class_name)
+        self.classId = ClassMasteryData.get_class_id(self)
+
+    #def collect_all(self):
+        #function to collect all individual coursework into a list
+
+    #def get_courseWorkId(self):
+        
+
+
+class Submission(Classwork):
+    def __init__(self, class_name):
+        Classwork.__init__(self, class_name)
+        self.draftGrade =self.get_draftGrade()
+        self.updateTime = self.get_updateTime()
+        self.alternateLink = self.get_alternateLink()
+        self.userId = self.get_userId()
+        self.creationTime = self.get_creationTime()
+        self.state = self.get_state()
+        self.courseWorkId = self.get_courseWorkId()
+        self.courseWorkType = self.get_courseWorkType()
+        self.assignedGrade = self.get_assignedGrade()
+        self.id = self.get_id()
+        self.maxPoints = self.get_maxPoints()    
+        self.courseid = classmasterydata.get_class_id()
+        self.classId = Classwork.get_class_id(self)
+    
+    def get_draftGrade(self):
+        return json.dumps(self.submissions)['draftGrade']
+       
+    def get_updateTime(self):
+        return json.dumps(self.submissions)['updateTime']
+
+    def get_alternateLink(self):
+        return json.dumps(self.submissions)['alternateLink']
+
+    def get_userId(self):
+        return json.dumps(self.submissions)['userId']
+
+    def get_creationTime(self):
+        return json.dumps(self.submissions)['creationTime']
+
+    def get_state(self):
+        return json.dumps(self.submissions)['state']
+   
+    def get_courseWorkId(self):
+        return json.dumps(self.submissions)['courseWorkId']
+   
+    def get_courseWorkType(self):
+        return json.dumps(self.submissions)['courseWorkType']
+  
+    def get_assignedGrade(self):
+        return json.dumps(self.submissions)['assignedGrade']
+
+    def get_id(self):
+        return json.dumps(self.submissions)['id']
+  
+    def get_maxPoints(self):
+        return json.dumps(self.submissions)['gradeHistory']['maxPoints']
+    
+    
 
 def get_courses():
     raw_response_data = service.courses().list().execute()
@@ -79,6 +146,9 @@ def get_submissions(course_ids, coursework_ids):
                     time.sleep(5)
                 else: raise
 
+
+
+
 #the following will spit out a list of all submissions!
 #courses = get_courses()
 #ids = isolate_ids(courses)
@@ -87,6 +157,12 @@ def get_submissions(course_ids, coursework_ids):
 #raw_submissions = get_submissions(ids, cw_ids)
 #submissions = raw_submissions[0]['studentSubmissions']
 
-testclass = ClassMasteryData('Code Nation Test')
+"""testclass = ClassMasteryData('Code Nation Test')
+testclass2 = ClassMasteryData('Opportunity Hack Dummy')
+c2 = service.courses().get(id=testclass2.id).execute()
+c = service.courses().get(id=testclass.id).execute()
+print(testclass.class_name , c['name'])
+print(testclass2.class_name, c2['name'])"""
 
-print(testclass.class_name)
+mySubmission = Submission('Code Nation Test')
+print(mySubmission.id, mySubmission.assignedGrade, mySubmission.maxPoints)
