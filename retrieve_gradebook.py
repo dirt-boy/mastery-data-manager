2c843710c62999e8c3a3337eae11476720ef3b9e
dirt-boy <gabriel.v.gagnon@gmail.com>
2020-03-05 16:42:41 -0800

    theres an emergency in the building..

--- retrieve_gradebook.py
+++ retrieve_gradebook.py
@@ -14,12 +14,10 @@ SCOPES = ['https://www.googleapis.com/auth/classroom.coursework.students https:/
 
 service = Create_Service(CLIENT_SECRET_FILE, API_SERVICE_NAME, API_VERSION, SCOPES)
 
-class ClassMasteryData:
+class Course:
     def __init__(self,class_name):
         self.class_name = class_name
         self.id = self.get_class_id()
-        self.coursework = self.get_coursework()
-        self.submissions = self.get_submissions(self.coursework)
 
     def get_class_id(self):
         my_courses = get_courses()
@@ -29,77 +27,107 @@ class ClassMasteryData:
         return self.id
         
     def get_coursework(self):
+        courseworks = []
         aslist = [self.id]
         self.coursework = get_coursework(aslist)
+        for course in self.coursework[0]['courseWork']:
+            courseworks.append(CourseWork(self.class_name, course['id']))
+        self.coursework = courseworks
         return self.coursework
 
     #this needs to be edited to return a dict or list of submissions tied to each course
-    def get_submissions(self, coursework_id):
-        coursework_id_i = [coursework_id[0]['courseWork'][0]['id']]
-        self.submissions = get_submissions([self.id], coursework_id_i)
+    def get_submissions(self, coursework):
+        #coursework_id = [coursework[0]['courseWork'][0]['id']]
+        l_submissions = []
+        coursework_ids = isolate_cw_ids(coursework)
+        for id in coursework_ids:
+            l_submissions.append(service.courses().courseWork().studentSubmissions().list(courseId=self.id, courseWorkId=id).execute())
+        self.submissions = l_submissions
         return self.submissions
 
-class Classwork(ClassMasteryData):
-    def __init__(self, class_name):
-        ClassMasteryData.__init__(self, class_name)
-        self.classId = ClassMasteryData.get_class_id(self)
+class CourseWork(Course):
+    def __init__(self, class_name, courseWorkId):
+        Course.__init__(self, class_name)
+        self.courseId = Course.get_class_id(self)
+        self.courseWorkId = courseWorkId
+        self.l_submissions = []
+        self.updateTime = self.get_updateTime()
+        self.assigneeMode = self.get_assigneeMode()
+        self.creatorUserId = self.get_creatorUserId()
+        self.dueDate = self.get_dueDate()
+        self.state = self.get_state()
+        self.dueTime = self.get_dueTime()
+        self.topicId = self.get_topicId()
+        self.description = self.get_description()
+        self.studentWorkFolder = self.get_studentWorkFolder()
+        self.title = self.get_title()
+        self.maxPoints = self.get_maxPoints()
+        self.workType = self.get_workType()
+        self.alternateLink = self.get_alternateLink()
+        self.materials = self.get_materials()
+        
+    def 
 
-    #def collect_all(self):
-        #function to collect all individual coursework into a list
 
-    #def get_courseWorkId(self):
+    def make_submissions(self):
+        l_submissions = []
+        for submission in self.submissions[0]['studentSubmissions']:
+            l_submissions.append(Submission(self.class_name, submission['id']))
+        self.l_submissions = l_submissions
+
+    def describe(self):
+        print("Class Name: ", self.class_name)
+        print("Course Work: ", self.coursework)
+        print("Submissions: ", self.l_submissions)
+      
+         
+
         
 
 
-class Submission(Classwork):
-    def __init__(self, class_name):
-        Classwork.__init__(self, class_name)
+class Submission(CourseWork):
+    def __init__(self, class_name, s_id):
+        CourseWork.__init__(self, class_name)
         self.draftGrade =self.get_draftGrade()
         self.updateTime = self.get_updateTime()
         self.alternateLink = self.get_alternateLink()
         self.userId = self.get_userId()
         self.creationTime = self.get_creationTime()
         self.state = self.get_state()
-        self.courseWorkId = self.get_courseWorkId()
         self.courseWorkType = self.get_courseWorkType()
         self.assignedGrade = self.get_assignedGrade()
-        self.id = self.get_id()
         self.maxPoints = self.get_maxPoints()    
-        self.courseId = self.get_class_id()
-        self.classId = Classwork.get_class_id(self)
-    
+        self.s_id = s_id        
+
     def get_draftGrade(self):
-        return self.submissions[0]['studentSubmissions'][0]['draftGrade']
+        return get_studentSubmission(self.classId, self.courseWorkId, self.id)[0]['studentSubmissions'][0]['draftGrade']
        
     def get_updateTime(self):
-        return self.submissions[0]['studentSubmissions'][0]['updateTime']
+        return get_studentSubmission(self.classId, self.courseWorkId, self.id)[0]['studentSubmissions'][0]['updateTime']
 
     def get_alternateLink(self):
-        return self.submissions[0]['studentSubmissions'][0]['alternateLink']
+        return get_studentSubmission(self.classId, self.courseWorkId, self.id)[0]['studentSubmissions'][0]['alternateLink']
 
     def get_userId(self):
-        return self.submissions[0]['studentSubmissions'][0]['userId']
+        return get_studentSubmission(self.classId, self.courseWorkId, self.id)[0]['studentSubmissions'][0]['userId']
 
     def get_creationTime(self):
-        return self.submissions[0]['studentSubmissions'][0]['creationTime']
+        return get_studentSubmission(self.classId, self.courseWorkId, self.id)[0]['studentSubmissions'][0]['creationTime']
 
     def get_state(self):
-        return self.submissions[0]['studentSubmissions'][0]['state']
+        return get_studentSubmission(self.classId, self.courseWorkId, self.id)[0]['studentSubmissions'][0]['state']
    
     def get_courseWorkId(self):
-        return self.submissions[0]['studentSubmissions'][0]['courseWorkId']
+        return get_studentSubmission(self.classId, self.courseWorkId, self.id)[0]['studentSubmissions'][0]['courseWorkId']
    
     def get_courseWorkType(self):
-        return self.submissions[0]['studentSubmissions'][0]['courseWorkType']
+        return get_studentSubmission(self.classId, self.courseWorkId, self.id)[0]['studentSubmissions'][0]['courseWorkType']
   
     def get_assignedGrade(self):
-        return self.submissions[0]['studentSubmissions'][0]['assignedGrade']
+        return get_studentSubmission(self.classId, self.courseWorkId, self.id)[0]['studentSubmissions'][0]['assignedGrade']
 
-    def get_id(self):
-        return self.submissions[0]['studentSubmissions'][0]['id']
-  
     def get_maxPoints(self):
-        return self.submissions[0]['studentSubmissions'][0]['submissionHistory'][2]['gradeHistory']['maxPoints']
+        return get_studentSubmission(self.classId, self.courseWorkId, self.id)[0]['studentSubmissions'][0]['submissionHistory'][2]['gradeHistory']['maxPoints']
     
     
 
@@ -110,6 +138,16 @@ def get_courses():
     for course in response_data:
         l_response.append((course['name'], course['id']))
     return l_response
+
+def get_course(id):
+    return service.courses().get(id=id).execute()
+
+def get_courseWork(courseId, id):
+    return service.courses().courseWork().get(courseId=courseId, id=id).execute()
+
+def get_studentSubmission(courseId, courseWorkId, id):
+    return service.courses().courseWork().studentSubmissions().get(courseId=courseId, courseWorkId=courseWorkId, id=id)
+
 
 def isolate_ids(courses):
     ids = []
@@ -126,9 +164,8 @@ def get_coursework(course_ids):
 
 def isolate_cw_ids(coursework):
     ids = []
-    for work in coursework:
-        if 'courseWork' in work.keys():
-            ids.append(work['courseWork'][0]['id'])
+    for work in coursework[0]['courseWork']:
+        ids.append(work['id'])
     return ids
 
 def get_submissions(course_ids, coursework_ids):
@@ -154,20 +191,12 @@ def get_submissions(course_ids, coursework_ids):
 #raw_submissions = get_submissions(ids, cw_ids)
 #submissions = raw_submissions[0]['studentSubmissions']
 
-#testclass = ClassMasteryData('Code Nation Test')
-#testclass2 = ClassMasteryData('Opportunity Hack Dummy')
-#c2 = service.courses().get(id=testclass2.id).execute()
-#c = service.courses().get(id=testclass.id).execute()
-#print(testclass.class_name , c['name'])
-#print(testclass2.class_name, c2['name'])
+myCourse = Course('Code Nation Test')
 
-#testclass = ClassMasteryData('Code Nation Test')
-#print(testclass.submissions)
-
-mySubmission = Submission('Code Nation Test')
-print(mySubmission.id, mySubmission.assignedGrade, mySubmission.maxPoints)
-
-
-
-
-
+def describe(course):
+    courseworks = myCourse.get_coursework()
+    for coursework in courseworks:
+        print coursework
+   
+       
+describe(myCourse) 
