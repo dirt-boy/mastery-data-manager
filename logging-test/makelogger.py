2e0614aae5ec85d072900878f091e47e10837c3a
dirt-boy <gabriel.v.gagnon@gmail.com>
2020-06-24 01:37:23 -0700

    created rubric_reader. currently this module is capable of taking an existing text file, reading it, and then writing a slice of the original file to a new file appended with '-rubric-data'.

---
+++ logging-test/makelogger.py
@@ -0,0 +1,45 @@
+import logging
+import sys
+from logging.handlers import TimedRotatingFileHandler
+FORMATTER = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+LOG_FILE = "my_app.log"
+
+def get_console_handler():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    return console_handler
+
+def get_file_handler():
+    file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
+    file_handler.setFormatter(FORMATTER)
+    return file_handler
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(get_console_handler())
+    logger.addHandler(get_file_handler())
+
+    logger.propagate = False
+    return logger
+
+
+my_logger = get_logger("my module name")
+my_logger.debug("a debug message!!!")
+
+
+
+"""
+gclass_logger = logging.getLogger('gclass_logger')
+
+console_handler = logging.StreamHandler()
+gclass_logger.addHandler(console_handler)
+
+gclass_logger.setLevel(logging.INFO)
+gclass_logger.info("you will see this")
+"""
+
+
+
+
+
