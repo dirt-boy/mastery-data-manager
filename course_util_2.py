976420092edb601c282b6a518558d797129be92a
dirt-boy <gabriel.v.gagnon@gmail.com>
2020-06-29 01:53:52 -0700

    just working out the kinks. then we slurp up the logs.

--- course_util_2.py
+++ course_util_2.py
@@ -14,28 +14,17 @@ SCOPES = ['https://www.googleapis.com/auth/classroom.coursework.students https:/
 classroom = Create_Service(CLIENT_SECRET_FILE, API_SERVICE_NAME, API_VERSION, SCOPES)
 
 def user_courses():
-    courses = classroom.courses().list().execute()
-    userId =  courses['courses'][0]['ownerId']
-    courseIds = []
-
-    for c in courses['courses']:
-        courseIds.append(c['id'])
-    print([userId, courseIds])
-    return [userId, courseIds]
+    courses = classroom.courses().list(fields='courses/name,courses/id').execute() 
+    return courses
 
 def course_coursework(courseId):
-    coursework = classroom.courses().courseWork().list(courseId=courseId).execute()
-    courseworkIds = []
-    for c in coursework['courseWork']:
-        courseworkIds.append(c['id'])
-    return courseworkIds
+    courseWork = classroom.courses().courseWork().list(courseId=courseId, fields='courseWork/id,courseWork/title,courseWork/maxPoints,courseWork/description,courseWork/creationTime,courseWork/alternateLink').execute()
+    return courseWork
 
 def coursework_submissions(courseId, courseworkId):    
-    submissions = classroom.courses().courseWork().studentSubmissions().list(courseId=courseId, courseWorkId=courseworkId).execute()
-    submissionIds = []
-    for s in submissions['studentSubmissions']:
-        submissionIds.append(s['id'])
-    return submissionIds
+    submissions = classroom.courses().courseWork().studentSubmissions().list(courseId=courseId, courseWorkId=courseworkId, fields='studentSubmissions/id, studentSubmissions/userId,studentSubmissions/assignedGrade,studentSubmission/alternateLink').execute()
+    return submissions
+
 
 def student_roster(courseId):
     students = classroom.list(courseId)
@@ -46,51 +35,31 @@ def teacher_roster(courseId):
     return teachers
 
 def course_dict():
-    course_dict = {}
-    courses = user_courses()
-    userId = courses[0]
-    course_dict['userId'] = userId
-    course_dict['userCourses'] = []
-    course_ids = courses[1]
+    course_dict = user_courses()['courses']
 
-    for i,cid in enumerate(course_ids):
-        coursework = course_coursework(cid)
-        course_name = classroom.courses().get(id=cid).execute()['name']
-        course_dict['userCourses'].append({'name':course_name, 'id':cid, 'coursework': []})
-        for j,cwid in enumerate(coursework):
-            coursework_name = classroom.courses().courseWork().get(courseId=cid, id=cwid).execute()['title']
-            course_dict['userCourses'][i]['coursework'].append({'title': coursework_name, 'courseWorkId': cwid, 'content':[] })
-            submissions = coursework_submissions(courseId=cid, courseworkId=cwid)
-            for k,sid in enumerate(submissions):
-                studentId = classroom.courses().courseWork().studentSubmissions().get(courseId=cid, courseWorkId=cwid, id=sid).execute()['userId']
-                try:
-                    grade = classroom.courses().courseWork().studentSubmissions().get(courseId=cid, courseWorkId=cwid, id=sid).execute()['assignedGrade']
-                except:
-                    print('No assigned grade available for course "'+coursework_name+', submission '+ str(studentId)+'. Continuing construction...')
-                    if(k%2==0):
-                        "Working...."
-                    else:
-                        "still.... working... dont worry..."
-                    grade = 'Ungraded'
-                finally:
+    for i,c in enumerate(course_dict):
 
-                    alternateLink = classroom.courses().courseWork().studentSubmissions().get(courseId=cid, courseWorkId=cwid, id=sid).execute()['alternateLink']
-                    course_dict['userCourses'][i]['coursework'][j]['content'].append({'studentId':studentId, 'assignedGrade':grade, 'alternateLink':alternateLink})
+        c['courseWork'] = []
+        c['courseWork'].append(course_coursework(c['id']))
 
+        for j,cw in enumerate(course_dict[i]['courseWork']):
+            
+            cw['studentSubmissions'] = []
+            cw['studentSubmissions'].append(coursework_submissions(c['courseWork'][i], cw['id']))
 
+    print(course_dict)
     return course_dict
 
 
 
 def write_file(course_dict):
-    name = str(user_courses()[0])+'.json' 
-    with open(name, 'x') as coursefile:
-        coursefile.write(course_dict)
-    print("New Course File written: "+name+".\nPlease check current directory for results.")
+    name = str('courses')+'.json' 
+    with open(name, 'w') as coursefile:
+        coursefile.write(json.dumps(course_dict))
     return coursefile
-    
+
+
 write_file(course_dict())
-    
 
 
 
